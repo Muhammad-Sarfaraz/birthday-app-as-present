520d77f4084055a2fa43621da18571d4d90c1d3d
id.sent.me@gmail.com <id.sent.me@gmail.com>
2021-12-16 23:29:22 +0600

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, TouchableOpacity, Text, View, Image } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    result: 0,
+    button_text: 'Click Here'
+  }
+
+  flipCard (value) {
+    if (value == 1) {
+      this.setState({
+        button_text: 'Hoise Ar chap dea lagbe na.',
+        result: 0
+      })
+    } else {
+      this.setState({
+        button_text: 'Happy Birthday Siam',
+        result: 1
+      })
+    }
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        {this.state.result == 0 ? (
+          <Image
+            source={require('./assets/first_image.gif')}
+            style={{
+              width: 350,
+              height: 350
+            }}
+          />
+        ) : (
+          <Image
+            source={require('./assets/siam.gif')}
+            style={{
+              width: 350,
+              height: 350
+            }}
+          />
+        )}
+
+        <TouchableOpacity
+          onPress={() => {
+            this.flipCard(this.state.result)
+          }}
+          style={styles.button}
+        >
+          <Text>{this.state.button_text}</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  button: {
+    backgroundColor: '#f1c40f',
+    padding: 20,
+    margin: 10,
+    borderRadius: 4
+  }
+})
